ade9c1a9a313394b2a172f66e33efe1f34757c8b
user@krolik-note <user@krolik-note>
2026-05-27 23:10:13 +0800

    add drawtext and paint form with gray color

--- tstclk/APPWND.C
+++ tstclk/APPWND.C
@@ -1,16 +1,10 @@
 
-// Contains the necessary headers and definitions for this program
 #include <stdio.h>
 #include <string.h>
 #include <commdlg.h>
 #include "appwnd.h"
 #include "res.h"
 
-/***  Global Variables  ***/
-char szName[256];
-//COLORREF crColor;
-HFONT hfFont;
-//BOOL tfFontLoaded;
 static HINSTANCE hInst = 0;	// current instance
 static int nCmdShow;
 /**********************************************************************/
@@ -38,9 +32,9 @@ BOOL AppWnd_register(HINSTANCE hInstance)
 	wc.hInstance = hInstance;	// Application that owns the class.
 	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = GetStockObject(WHITE_BRUSH);
-	wc.lpszMenuName = "CMDLGAPMENU";	// Name of menu resource in .RC file.
-	wc.lpszClassName = "CMDLG";	// Name used in call to CreateWindow.
+	wc.hbrBackground = GetStockObject(GRAY_BRUSH);
+	wc.lpszMenuName = "TESTCLKMENU";	// Name of menu resource in .RC file.
+	wc.lpszClassName = "TESTCLK";	// Name used in call to CreateWindow.
 
 	/* Register the window class and return success/failure code. */
 
@@ -55,63 +49,59 @@ BOOL AppWnd_register(HINSTANCE hInstance)
 long FAR PASCAL _export MainWndProc(HWND hWnd, unsigned message,
 									WORD wParam, LONG lParam)
 {
-	HFONT fTemp;	// Placeholder for the original font
-	RECT rTemp;     // Client are needed by DrawText()
-	HDC hdc;        // HDC for Window
-	PAINTSTRUCT ps;	// Paint Struct for BeginPaint call
+    HFONT fTemp;	// Placeholder for the original font
+    RECT rTemp;     // Client are needed by DrawText()
+    HDC hdc;        // HDC for Window
+    PAINTSTRUCT ps;	// Paint Struct for BeginPaint call
 
-	switch (message) {
-	case WM_CREATE: // Initialize Global vars
-			strcpy( szName, "" );           // Empty the file name string
-		/*	crColor = RGB( 0, 0, 0 );       // Use black as the default color
-			hfFont = 0;                     // Empty the handle to the font
-			tfFontLoaded = FALSE;           // Set the font selected flag to false
-		*/
-		return NULL;
+    switch (message) {
+        case WM_CREATE: // Initialize Global vars
+            break;
 
-	case WM_PAINT:
-	// Display the file name using the selected font in the selected color.
+        case WM_PAINT:
+            // Display the file name using the selected font in the selected color.
 
-			hdc=BeginPaint(hWnd,&ps);
-			/*SetTextColor( hdc, crColor );
-			if( tfFontLoaded == TRUE )
-				fTemp = (HFONT)SelectObject( hdc, hfFont );
-			GetClientRect( hWnd, &rTemp );
-			DrawText( hdc, szName, strlen( szName ), &rTemp, DT_CENTER | DT_WORDBREAK );
-			if( tfFontLoaded == TRUE )
-				SelectObject( hdc, fTemp );
-			*/
-			EndPaint(hWnd,&ps);
-		break;
+            hdc=BeginPaint(hWnd, &ps);
+            GetClientRect( hWnd, &rTemp );
+            rTemp.right  = 75;
+            rTemp.bottom = 20;
+            DrawText( hdc, "Hello world", 11, &rTemp, DT_CENTER );
+            /*SetTextColor( hdc, crColor );
+              if( tfFontLoaded == TRUE )
+              fTemp = (HFONT)SelectObject( hdc, hfFont );
+              
+              if( tfFontLoaded == TRUE )
+              SelectObject( hdc, fTemp );
+              */
+            EndPaint(hWnd,&ps);
+            break;
 
-	case WM_COMMAND:	// message: command from application menu
-		switch(wParam)
-		{
-			case CM_EXIT:
-					DestroyWindow(hWnd);
-				break;
+        case WM_COMMAND:	// message: command from application menu
+            switch(wParam)
+            {
+                case CM_EXIT:
+                    DestroyWindow(hWnd);
+                    break;
 
-		
-			case CM_U_HELPABOUT:
-					MessageBox(hWnd,szCMDLGAPAbout,"About CMDLG",MB_OK);
-				break;
 
-			default:
-				break;
-		}
-		break;
+                case CM_U_HELPABOUT:
+                    MessageBox(hWnd,szCMDLGAPAbout,"About CMDLG",MB_OK);
+                    break;
 
-	case WM_QUIT:
-	case WM_DESTROY:	// message: window being destroyed
-			if( hfFont != 0 )
-				DeleteObject( hfFont );
-			PostQuitMessage(0);
-		break;
+                default:
+                    break;
+            }
+            break;
 
-	default:			// Passes it on if unproccessed
-		return (DefWindowProc(hWnd, message, wParam, lParam));
-	}
-	return (NULL);
+        case WM_QUIT:
+        case WM_DESTROY:	// message: window being destroyed
+	    PostQuitMessage(0);
+            break;
+
+        default:			// Passes it on if unproccessed
+            return (DefWindowProc(hWnd, message, wParam, lParam));
+    }
+    return (NULL);
 }
 
 BOOL AppWnd_create(struct AppWnd* wnd, HINSTANCE hInstance, int _cmd)
@@ -125,7 +115,7 @@ BOOL AppWnd_create(struct AppWnd* wnd, HINSTANCE hInstance, int _cmd)
     
 	/* Create a main window for this application instance.  */
 	hWnd = CreateWindow(
-		"CMDLG", 	               // See RegisterClass() call.
+		"TESTCLK", 	               // See RegisterClass() call.
 		"Test of machine speed",       // Text for window title bar.
 		WS_OVERLAPPEDWINDOW,	       // Window style.
 		CW_USEDEFAULT, 		       // Default horizontal position.
