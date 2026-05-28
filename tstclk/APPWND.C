73f8dd0050477174545fc3497fe0383edb7ef93a
Sergey Ryabchenkov(freevm) <ihtiandr9@yandex.ru>
2026-05-28 15:27:55 +0800

    add ok button with close command

--- tstclk/APPWND.C
+++ tstclk/APPWND.C
@@ -7,38 +7,93 @@
 
 static HINSTANCE hInst = 0;	// current instance
 static int nCmdShow;
+static HWND hBtnClose;
 /**********************************************************************/
 // { Forward declarations
 WORD AppWnd_run(struct AppWnd* wnd);
 // }
 /**********************************************************************/
+// Default event handlers (replicate original behavior)
+
+static void AppWnd_OnCreate(struct AppWnd* Sender, LPCREATESTRUCT CreateStruct)
+{
+    RECT rc;
+    GetClientRect(Sender->hWnd, &rc);
+    hBtnClose = CreateWindow(
+        "BUTTON", "&Close",
+        WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        (rc.right - 80) / 2, (rc.bottom - 30) / 2,
+        80, 30,
+        Sender->hWnd,
+        (HMENU)CM_EXIT,
+        CreateStruct->hInstance,
+        NULL
+    );
+}
+
+static void AppWnd_OnPaint(struct AppWnd* Sender)
+{
+    RECT rTemp;
+    HDC hdc;
+    PAINTSTRUCT ps;
+
+    hdc = BeginPaint(Sender->hWnd, &ps);
+    GetClientRect(Sender->hWnd, &rTemp);
+    rTemp.right  = 75;
+    rTemp.bottom = 20;
+    DrawText(hdc, "Hello world", 11, &rTemp, DT_CENTER);
+    EndPaint(Sender->hWnd, &ps);
+}
+
+static void AppWnd_OnCommand(struct AppWnd* Sender, WORD Cmd, HWND hwndCtl)
+{
+    switch (Cmd)
+    {
+        case CM_EXIT:
+            DestroyWindow(Sender->hWnd);
+            break;
+
+        case CM_U_HELPABOUT:
+            MessageBox(Sender->hWnd, szCMDLGAPAbout, "About CMDLG", MB_OK);
+            break;
+
+        default:
+            break;
+    }
+}
+
+static void AppWnd_OnDestroy(struct AppWnd* Sender)
+{
+    PostQuitMessage(0);
+}
+
 BOOL AppWnd_register(HINSTANCE hInstance)
 {
-	WNDCLASS  wc;
+    WNDCLASS  wc;
 
-    if(hInst)
+    if (hInst)
         return TRUE;
 
     hInst = hInstance;
 
-	// Fill in window class structure with parameters that describe the
-	// main window.
+    // Fill in window class structure with parameters that describe the
+    // main window.
 
-	wc.style = CS_HREDRAW | CS_VREDRAW;	// Class style(s).
-	wc.lpfnWndProc = (long (FAR PASCAL*)())MainWndProc;	// Function to retrieve messages for
-														// windows of this class.
-	wc.cbClsExtra = 0;	// No per-class extra data.
-	wc.cbWndExtra = 0;	// No per-window extra data.
-	wc.hInstance = hInstance;	// Application that owns the class.
-	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = GetStockObject(GRAY_BRUSH);
-	wc.lpszMenuName = "TESTCLKMENU";	// Name of menu resource in .RC file.
-	wc.lpszClassName = "TESTCLK";	// Name used in call to CreateWindow.
+    wc.style = CS_HREDRAW | CS_VREDRAW;	// Class style(s).
+    wc.lpfnWndProc = (long (FAR PASCAL*)())MainWndProc;	// Function to retrieve messages for
+                                                        // windows of this class.
+    wc.cbClsExtra = 0;	// No per-class extra data.
+    wc.cbWndExtra = sizeof(LONG);	// Per-window extra data for AppWnd* pointer
+    wc.hInstance = hInstance;	// Application that owns the class.
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = GetStockObject(GRAY_BRUSH);
+    wc.lpszMenuName = "TESTCLKMENU";	// Name of menu resource in .RC file.
+    wc.lpszClassName = "TESTCLK";	// Name used in call to CreateWindow.
 
-	/* Register the window class and return success/failure code. */
+    /* Register the window class and return success/failure code. */
 
-	return (RegisterClass(&wc));
+    return (RegisterClass(&wc));
 
 }
 
@@ -47,55 +102,51 @@ BOOL AppWnd_register(HINSTANCE hInstance)
 	FUNCTION: MainWndProc(HWND, unsigned, WORD, LONG)
 ****************************************************************************/
 long FAR PASCAL _export MainWndProc(HWND hWnd, unsigned message,
-									WORD wParam, LONG lParam)
+                                    WORD wParam, LONG lParam)
 {
-    HFONT fTemp;	// Placeholder for the original font
-    RECT rTemp;     // Client are needed by DrawText()
-    HDC hdc;        // HDC for Window
-    PAINTSTRUCT ps;	// Paint Struct for BeginPaint call
+    struct AppWnd* wnd;
 
-    switch (message) {
-        case WM_CREATE: // Initialize Global vars
+    switch (message)
+    {
+        case WM_CREATE:	// Initialize window: store AppWnd*, call OnCreate
+        {
+            LPCREATESTRUCT lpcs = (LPCREATESTRUCT)lParam;
+            wnd = (struct AppWnd*)lpcs->lpCreateParams;
+            SetWindowLong(hWnd, 0, (LONG)wnd);
+            wnd->hWnd = hWnd;
+            if (wnd->OnCreate)
+                wnd->OnCreate(wnd, lpcs);
+            return 0;
+        }
+
+        default:
             break;
+    }
 
+    wnd = (struct AppWnd*)GetWindowLong(hWnd, 0);
+    if (!wnd)
+        return DefWindowProc(hWnd, message, wParam, lParam);
+
+    switch (message)
+    {
         case WM_PAINT:
-            // Display the file name using the selected font in the selected color.
-
-            hdc=BeginPaint(hWnd, &ps);
-            GetClientRect( hWnd, &rTemp );
-            rTemp.right  = 75;
-            rTemp.bottom = 20;
-            DrawText( hdc, "Hello world", 11, &rTemp, DT_CENTER );
-            /*SetTextColor( hdc, crColor );
-              if( tfFontLoaded == TRUE )
-              fTemp = (HFONT)SelectObject( hdc, hfFont );
-              
-              if( tfFontLoaded == TRUE )
-              SelectObject( hdc, fTemp );
-              */
-            EndPaint(hWnd,&ps);
+            if (wnd->OnPaint)
+                wnd->OnPaint(wnd);
+            else
+                return DefWindowProc(hWnd, message, wParam, lParam);
             break;
 
         case WM_COMMAND:	// message: command from application menu
-            switch(wParam)
-            {
-                case CM_EXIT:
-                    DestroyWindow(hWnd);
-                    break;
-
-
-                case CM_U_HELPABOUT:
-                    MessageBox(hWnd,szCMDLGAPAbout,"About CMDLG",MB_OK);
-                    break;
-
-                default:
-                    break;
-            }
+            if (wnd->OnCommand)
+                wnd->OnCommand(wnd, wParam, (HWND)LOWORD(lParam));
+            else
+                return DefWindowProc(hWnd, message, wParam, lParam);
             break;
 
-        case WM_QUIT:
         case WM_DESTROY:	// message: window being destroyed
-	    PostQuitMessage(0);
+            if (wnd->OnDestroy)
+                wnd->OnDestroy(wnd);
+            PostQuitMessage(0);
             break;
 
         default:			// Passes it on if unproccessed
@@ -108,37 +159,42 @@ BOOL AppWnd_create(struct AppWnd* wnd, HINSTANCE hInstance, int _cmd)
 {
 
     HWND hWnd;
+
+    // Set default event handlers
+    wnd->OnCreate  = AppWnd_OnCreate;
+    wnd->OnPaint   = AppWnd_OnPaint;
+    wnd->OnCommand = AppWnd_OnCommand;
+    wnd->OnDestroy = AppWnd_OnDestroy;
+
     nCmdShow = _cmd;
     wnd->run = AppWnd_run;
 
     AppWnd_register(hInstance);
-    
-	/* Create a main window for this application instance.  */
-	hWnd = CreateWindow(
-		"TESTCLK", 	               // See RegisterClass() call.
-		"Test of machine speed",       // Text for window title bar.
-		WS_OVERLAPPEDWINDOW,	       // Window style.
-		CW_USEDEFAULT, 		       // Default horizontal position.
-		CW_USEDEFAULT,	   	       // Default vertical position.
-		CW_USEDEFAULT,	      	       // Default width.
-		CW_USEDEFAULT, 		       // Default height.
-		NULL,		   	       // Overlapped windows have no parent.
-		NULL,	   		       // Use the window class menu.
-		hInst,  		       // This instance owns this window.
-		NULL			       // Pointer not needed.
-	);
 
-	/* If window could not be created, return "failure" */
+    /* Create a main window for this application instance.  */
+    hWnd = CreateWindow(
+        "TESTCLK", 	                   // See RegisterClass() call.
+        "Test of machine speed",       // Text for window title bar.
+        WS_OVERLAPPEDWINDOW,	       // Window style.
+        CW_USEDEFAULT, 		       // Default horizontal position.
+        CW_USEDEFAULT,	   	       // Default vertical position.
+        CW_USEDEFAULT,	      	       // Default width.
+        CW_USEDEFAULT, 		       // Default height.
+        NULL,		   	       // Overlapped windows have no parent.
+        NULL,	   		       // Use the window class menu.
+        hInst,  		       // This instance owns this window.
+        wnd			       // Pointer to AppWnd (lpCreateParams)
+    );
 
-	if (!hWnd)
-		return (FALSE);
+    /* If window could not be created, return "failure" */
 
-	/* Make the window visible; update its client area; and return "success" */
+    if (!hWnd)
+        return (FALSE);
 
     wnd->hWnd = hWnd;
-	return (TRUE);				// Returns the value from PostQuitMessage
+    return (TRUE);				// Returns the value from PostQuitMessage
 
-} 
+}
 
 WORD AppWnd_run(struct AppWnd* wnd)
 {
@@ -157,5 +213,5 @@ WORD AppWnd_run(struct AppWnd* wnd)
         TranslateMessage(&msg);	// Translates virtual key codes
         DispatchMessage(&msg);	// Dispatches message to window
     }
-    return (msg.wParam);	
+    return (msg.wParam);
 }
